ec6cc6a050060c6c77309c415dcc5389d86fab0c
Stanislav Silnicki <stanislav.silnicki@mailgate.us>
2024-06-02 03:00:27 +0300

    cv1800: import & compile Milk-V Duo (RISC-V CV1800b CPU) third stage boot loader

--- build.zig
+++ build.zig
@@ -4,7 +4,9 @@ const CrossTarget = std.zig.CrossTarget;
 const Feature = @import("std").Target.Cpu.Feature;
 
 const stm32mp1 = @import("build-stm32mp1.zig");
+const cv1800b = @import("build-cv1800b.zig");
 
 pub fn build(b: *std.Build) void {
     stm32mp1.build(b);
+    cv1800b.build(b);
 }
