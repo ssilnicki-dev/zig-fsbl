24c1bd42ac7508a391f18902a02665086c68f1df
Stanislav Silnicki <stanislav.silnicki@mailgate.us>
2025-06-23 02:57:09 +0300

    stm32mp1: refactor armv7 registers accessors

--- src/stm32mp1/arch.zig
+++ src/stm32mp1/arch.zig
@@ -4,19 +4,46 @@ const armv7_general_register = enum(u4) { r0 = 0 };
 // TODO: add support for 64 bit width registers
 fn CP15Reg(comptime op1: u3, comptime crn: u4, comptime crm: u4, comptime op2: u3) type {
     return struct {
-        const Self = @This();
-        const r = armv7_general_register.r0;
-        pub inline fn readTo(comptime access_reg: armv7_general_register) void {
+        usingnamespace GenericAccessors(@This());
+        inline fn readTo(comptime access_reg: armv7_general_register) void {
             access(access_reg, .mrc);
         }
-        pub inline fn writeFrom(comptime access_reg: armv7_general_register) void {
+        inline fn writeFrom(comptime access_reg: armv7_general_register) void {
             access(access_reg, .mcr);
         }
         inline fn access(comptime access_reg: armv7_general_register, comptime instruction: @TypeOf(.@"enum")) void {
             asm volatile (print("{s} p15, {d}, r{d}, c{d}, c{d}, {d}", .{ @tagName(instruction), op1, @intFromEnum(access_reg), crn, crm, op2 }));
         }
+    };
+}
+
+fn FPReg(comptime register: @TypeOf(.@"enum")) type {
+    return struct {
+        usingnamespace GenericAccessors(@This());
+        inline fn readTo(comptime access_reg: armv7_general_register) void {
+            asm volatile (print("vmrs r{d}, {s}", .{ @intFromEnum(access_reg), @tagName(register) }));
+        }
+        inline fn writeFrom(comptime access_reg: armv7_general_register) void {
+            asm volatile (print("vmsr {s}, r{d}", .{ @tagName(register), @intFromEnum(access_reg) }));
+        }
+    };
+}
+
+fn GenericAccessors(self: type) type {
+    return struct {
+        const data = armv7_general_register.r0;
         fn Bit(comptime bit: u5) type {
             return Field(bit, 1, enum(u1) { Disabled = 0, Enabled = 1 });
+        }
+        fn Data32() type {
+            return struct {
+                pub inline fn readTo(comptime reg: armv7_general_register) void {
+                    self.readTo(reg);
+                }
+                pub inline fn writeFrom(comptime reg: armv7_general_register) void {
+                    self.writeFrom(reg);
+                }
+            };
         }
 
         fn Field(comptime shift: u5, comptime width: u5, comptime values: @TypeOf(enum {})) type {
@@ -25,26 +52,23 @@ fn CP15Reg(comptime op1: u3, comptime crn: u4, comptime crm: u4, comptime op2: u
                 pub inline fn Select(comptime v: values) void {
                     Set(@intFromEnum(v));
                 }
-                pub inline fn Set(comptime v: u32) void {
-                    comptime if (@bitSizeOf(@TypeOf(v)) - @clz(v) > width) {
-                        @compileError(print("Value {d} (0x{x}) can not fit into Field with length of {d} bits", .{ v, v, width }));
-                    };
+                pub inline fn Set(comptime v: u31) void {
                     if (v == 0) {
                         Clear();
                     } else {
-                        Self.readTo(r);
-                        asm volatile (print("and r{d}, r{d}, 0x{x}", .{ @intFromEnum(r), @intFromEnum(r), ~(((@as(u32, 1) << width) - 1) << shift) }));
-                        asm volatile (print("orr r{d}, r{d}, 0x{x}", .{ @intFromEnum(r), @intFromEnum(r), v << shift }));
-                        Self.writeFrom(r);
+                        self.readTo(data);
+                        asm volatile (print("bfc r{d}, #{d}, #{d}", .{ @intFromEnum(data), shift, width }));
+                        asm volatile (print("orr r{d}, r{d}, #{d}", .{ @intFromEnum(data), @intFromEnum(data), v << shift }));
+                        self.writeFrom(data);
                     }
                 }
                 pub inline fn Clear() void {
-                    Self.readTo(r);
-                    asm volatile (print("and r{d}, r{d}, 0x{x}", .{ @intFromEnum(r), @intFromEnum(r), ~(((@as(u32, 1) << width) - 1) << shift) }));
-                    Self.writeFrom(r);
+                    self.readTo(data);
+                    asm volatile (print("bfc r{d}, #{d}, #{d} ", .{ @intFromEnum(data), shift, width }));
+                    self.writeFrom(data);
                 }
                 pub inline fn readTo(comptime reg: armv7_general_register) void {
-                    Self.readTo(reg);
+                    self.readTo(reg);
                     asm volatile (print("ubfx r{d}, r{d}, {d}, {d}", .{ @intFromEnum(reg), @intFromEnum(reg), shift, width }));
                 }
             };
