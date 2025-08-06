28733e1c3d4bc311bad2d3f266a61ecfd50c1095
Stanislav Silnicki <stanislav.silnicki@mailgate.us>
2025-08-06 04:24:50 +0300

    stm32mp1: init RCC periphery

--- src/stm32mp1/plat.zig
+++ src/stm32mp1/plat.zig
@@ -1,15 +1,23 @@
 const arch = @import("arch.zig");
 const stm32mp157c = @import("stm32mp157c.zig");
 const PWR = stm32mp157c.PWR;
+const RCC = stm32mp157c.RCC;
 var pwr: PWR = undefined;
+var rcc: RCC = undefined;
 
 fn mapPeriphery() void {
     pwr = PWR{ .port = arch.mapPeriphery(0x50001000, 1024) catch |e| panic(@src().line, e) };
+    rcc = RCC{ .port = arch.mapPeriphery(0x50000000, 4096) catch |e| panic(@src().line, e) };
 }
 
 pub export fn Initialize() void {
     mapPeriphery();
     pwr.backupDomainWriteProtection(.Disable);
+    if (rcc.getRTCSource() == .NoClock) { // Coldboot
+        rcc.resetVSwitchDomain();
+    }
+    // TODO: setup clocks/dividers/plls/muxers -> mostly refactoring
+    rcc.enableCSI(); // required for automatic IO compensation
 }
 
 noinline fn panic(line: u32, err: anyerror) noreturn {
